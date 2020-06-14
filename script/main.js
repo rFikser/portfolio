4cea914109710f17286acdf62356f4cda28fa515
rFikser <john321cheba@gmail.com>
2020-06-14 12:30:12 +0300

    initial commit

---
+++ script/main.js
@@ -0,0 +1,25 @@
+// text type animation
+
+var typed = new Typed(".typed", {
+  strings: ["I'm Junior Frontend Developer", "I love to create"],
+  typeSpeed: 50,
+  loop: true,
+  loopCount: Infinity,
+});
+
+// fade effect on scroll
+
+AOS.init({
+  duration: 1500,
+});
+
+// active links toggle
+
+$(".header__menu .header__menu-item a").click(function () {
+  $("a").removeClass("active");
+  $(this).closest("a").addClass("active");
+});
+
+$(".header__list span").click(function () {
+  $(".header__menu .header__menu-item a").removeClass("active");
+});
